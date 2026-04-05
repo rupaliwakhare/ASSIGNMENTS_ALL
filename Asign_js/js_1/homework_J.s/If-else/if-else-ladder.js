f7dffb3d5d8feda6fe349318fad3d3c2d4e2cad5
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-05 20:36:36 +0530

    if-else-ladder

---
+++ Asign_js/js_1/homework_J.s/If-else/if-else-ladder.js
@@ -0,0 +1,18 @@
+let marks = 90;
+
+if(marks >=90){
+    console.log("First Class");
+    
+}else if(marks>=75){
+    console.log("Second class");
+    
+}else if(marks >=45){
+    console.log("third class");
+    
+}else if(marks >=35){
+    console.log("passed");
+    
+}else{
+    console.log("failed");
+    
+}
